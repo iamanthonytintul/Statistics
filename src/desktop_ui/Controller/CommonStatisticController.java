8cf1e03c6d0d7d3c964ae944010f1d0679769a08
Cappoocha <katerinacappuccino@gmail.com>
2017-06-16 01:58:10 +0400

    Desktop UI. Интерфейс пользователя.

---
+++ src/desktop_ui/Controller/CommonStatisticController.java
@@ -0,0 +1,15 @@
+package desktop_ui.Controller;
+
+import javafx.fxml.FXML;
+
+/**
+ * Created by Cappoocha on 16.06.2017.
+ */
+public class CommonStatisticController
+{
+    @FXML
+    private void setScene()
+    {
+        Main.setScene(Main.dailyStatistic);
+    }
+}
